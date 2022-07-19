d07ae381ef905bd025270c1dc9b3635385b9a3e4
Oussama Benyssef <OUSSAMA.BENYSSEF@taalim.ma>
2022-07-19 19:39:09 +0100

    Third Commit

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -34,26 +34,17 @@ export default function Page({post}){
     )
 }
   export async function getStaticPaths() {
-    // Call an external API endpoint to get posts
-    const res = await fetch('http://localhost:3000/posts');
+    const res = await fetch('http://localhost:3000/api/posts');
     const posts = await res.json();
-  
-    // Get the paths we want to pre-render based on posts
     const paths = posts.map((value) => ({
-      params: { postId: value.id},
+      params: { postId: value.id.toString()},
     }))
-  
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
     return { paths, fallback: false }
   }
 
   export async function getStaticProps({ params }) {
-    // params contains the post `id`.
-    // If the route is like /posts/1, then params.id is 1
-    const res = await fetch(`http://localhost:3000/posts/${params.id}`)
+    const res = await fetch(`http://localhost:3000/api/posts/${params.id}`)
     const post = await res.json()
   
-    // Pass post data to the page via props
     return { props: { post } }
   }
